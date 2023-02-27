d3cf455afb36dca1aaceafe46351b363c141cfff
MSAAVEDRA98 <simonsnow98@gmail.com>
2023-02-27 09:06:53 -0300

    Pequeños cambios realizados

--- src/app/Servicios/auth.guard.ts
+++ src/app/Servicios/auth.guard.ts
@@ -10,15 +10,12 @@ import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	constructor(
-		private authService: AuthService,
-		private router: Router) { }
-	canActivate(
-		route: ActivatedRouteSnapshot,
-		state: RouterStateSnapshot): boolean | Promise<boolean> {
+	constructor(private authService: AuthService, private router: Router) { }
+	
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 		var isAuthenticated = this.authService.logIn;
-		if (!isAuthenticated) {
-			this.router.navigate(['/login']);
+		if (isAuthenticated) {
+			this.router.navigate(['/perfil']);
 		}
 		return isAuthenticated;
 	}
